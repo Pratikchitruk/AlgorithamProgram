d94f76db679c294412f6e04d5071517603331cf0
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-17 11:26:11 +0530

    program.cs push for AlgorithamProgram

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithamProgram
+
+{
+    public class Program
+    {
+        
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to algoritham program");
+
+
+            Permutation.MyPermute();
+            Binarysearch.Binary();
+            InsertingSort.Sort();
+            BubbleSort.MyBubblesort();
+            MergeSort.MyMain();
+            Anaagram.Main();
+            PrimeNo.EnterPrimeNo();
+            VariousNo.EnterVariousNo();
+        }
+    }
+}
